10efc80ddfc1822c202caa73b1832f1cc1d26930
JeongMinGyu <poiuy745114@gmail.com>
2019-08-12 19:55:12 +0900

    2019-08-12

--- Personal_Information_Finder/Personal_Information_Finder/MainWindow.xaml.cs
+++ Personal_Information_Finder/Personal_Information_Finder/MainWindow.xaml.cs
@@ -12,17 +12,37 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
 
 namespace Personal_Information_Finder
 {
+    
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
     public partial class MainWindow : Window
     {
+        string Folderselect = "";
+        string[] FileEntries;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private string OpenClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog Folderopen = new FolderBrowserDialog();
+            Folderopen.Description = "폴더 검색";
+            Folderopen.ShowDialog();
+            Folderselect = Folderopen.SelectedPath;
+            FileEntries = Directory.GetFiles(Folderselect);
+            //폴더엔트리 값들을 리스트뷰에 집어넣음//
+        }
+
+        private void FindClick(object sender, RoutedEventArgs e)
+        {
+            foreach(string list in FileEntries)
         }
     }
 }
