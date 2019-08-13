bb480562be645ee786b8f891d28583bc33ef9b1d
poiuy745114 <poiuy745114@gmail.com>
2019-08-13 19:51:48 +0900

    2013-08-13

--- Personal_Information_Finder/Personal_Information_Finder/MainWindow.xaml.cs
+++ Personal_Information_Finder/Personal_Information_Finder/MainWindow.xaml.cs
@@ -25,24 +25,79 @@ namespace Personal_Information_Finder
     {
         string Folderselect = "";
         string[] FileEntries;
+        string[] SearchedFile;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private string OpenClick(object sender, RoutedEventArgs e)
+        #region open 클릭
+        private void OpenClick(object sender, RoutedEventArgs e)
         {
             FolderBrowserDialog Folderopen = new FolderBrowserDialog();
             Folderopen.Description = "폴더 검색";
             Folderopen.ShowDialog();
             Folderselect = Folderopen.SelectedPath;
             FileEntries = Directory.GetFiles(Folderselect);
-            //폴더엔트리 값들을 리스트뷰에 집어넣음//
         }
+        #endregion
 
+        #region find 클릭
         private void FindClick(object sender, RoutedEventArgs e)
         {
-            foreach(string list in FileEntries)
+            foreach (string list in FileEntries)
+            {
+                if (System.IO.Path.GetExtension(list) == ".txt")
+                {
+                    string view = "";
+                    string values = "";
+                    string fileread = File.ReadAllText(list);
+                    int FindNo_Value = FindNo(fileread, ref values);
+                    Console.WriteLine(FindNo_Value);
+                    Console.WriteLine(values);
+                    if (FindNo_Value > 0)
+                    {
+                        ListView.Items.Add(list);
+                    }
+                }
+            }
         }
+        #endregion
+
+        #region 전화번호 탐색 함수
+        public int FindNo(string input, ref string values)
+        {
+            string[] patterns =
+            {
+                @"(^|\W)01[01]\d{7,8}(\W|$)",
+                @"(^|\W)01[01]-\d{3,4}-\d{4}(\W|$)"
+            };
+
+            int count = 0;
+
+            foreach ( string pattern in patterns)
+            {
+                System.Text.RegularExpressions.MatchCollection matches = System.Text.RegularExpressions.Regex.Matches(input, pattern);
+
+                foreach (System.Text.RegularExpressions.Match m in matches)
+                {
+                    ++count;
+                    values += m + "\n";
+                }
+            }
+            return count;
+        }
+        #endregion
+
+        #region 리스트 선택
+        private void List_select(object sender, MouseButtonEventArgs e)
+        {
+            TextArea.Text = "";
+            System.Windows.Controls.ListView list = sender as System.Windows.Controls.ListView;
+            string convert = list.SelectedItem as string;
+            string value = File.ReadAllText(convert);
+            TextArea.Text = value;
+        }
+        #endregion
     }
 }
